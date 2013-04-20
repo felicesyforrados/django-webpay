1277f389ef6fe66d9c123ebfc0558249d97609b9
misael <arensiatik@hotmail.com>
2013-04-20 01:49:57 -0400

    Modificaciones nueva

--- webpay/views.py
+++ webpay/views.py
@@ -8,13 +8,11 @@ from datetime import datetime
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from webpay.models import OrdenCompraWebpay
 from webpay.conf import *
 from webpay.signals import pago_defectuoso
 
-@require_POST
 @csrf_exempt
 def compra_webpay(request):
     """
@@ -26,54 +24,57 @@ def compra_webpay(request):
     Validacion de orden de compra.
     """
     resp = RECHAZADO_RESPONSE
-    req = request
-    qs = _get_order_params(req)
-    orden_compra = req.POST.get('TBK_ORDEN_COMPRA')
-    respuesta = req.POST.get('TBK_RESPUESTA')
-    monto = int(req.POST.get('TBK_MONTO')) / 100
-    codigo_autorizacion = req.POST.get('TBK_CODIGO_AUTORIZACION')
-    id_transaccion = req.POST.get('TBK_ID_TRANSACCION')
-    final_num_tarjeta = req.POST.get('TBK_FINAL_NUMERO_TARJETA')
-    id_sesion = req.POST.get("TBK_ID_SESION")
-    tipo_pago = req.POST.get("TBK_TIPO_PAGO")
-    tipo_transaccion = req.POST.get("TBK_TIPO_TRANSACCION")
-    fecha_contable = req.POST.get("TBK_FECHA_CONTABLE")
-    numero_cuota = req.POST.get("TBK_NUMERO_CUOTAS")
-    #Comprueba archivo
-    try:
-        orden = OrdenCompraWebpay.objects.get(
-            orden_compra=orden_compra)
-        orden.id_transaccion = id_transaccion
-        orden.codigo_transaccion = respuesta
-        orden.codigo_autorizacion = codigo_autorizacion
-        orden.final_num_tarjeta = final_num_tarjeta
-        orden.id_sesion = id_sesion
-        orden.codigo_autorizacion = codigo_autorizacion
-        orden.fecha_transaccion = datetime.today()
-        orden.tipo_pago = tipo_pago
-        orden.tipo_transaccion = tipo_transaccion
-        orden.fecha_contable = fecha_contable
-        orden.numero_cuota = numero_cuota
-    except OrdenCompraWebpay.DoesNotExist:
-        data = [orden_compra, get_client_ip(req)]
-        pago_defectuoso.send(sender=data)
-        raise
-    if respuesta == "0":
-        if int(monto) == int(orden.monto):
-            #Valida MAC
-            if valida_mac(qs) == VALID_MAC_RESPONSE:
-                orden.status = STATUS["PAGADO"]
-                resp = ACEPTADO_RESPONSE
+    if request.method == "POST":
+        req = request
+        qs = _get_order_params(req)
+        orden_compra = req.POST.get('TBK_ORDEN_COMPRA')
+        respuesta = req.POST.get('TBK_RESPUESTA')
+        monto = int(req.POST.get('TBK_MONTO')) / 100
+        codigo_autorizacion = req.POST.get('TBK_CODIGO_AUTORIZACION')
+        id_transaccion = req.POST.get('TBK_ID_TRANSACCION')
+        final_num_tarjeta = req.POST.get('TBK_FINAL_NUMERO_TARJETA')
+        id_sesion = req.POST.get("TBK_ID_SESION")
+        tipo_pago = req.POST.get("TBK_TIPO_PAGO")
+        tipo_transaccion = req.POST.get("TBK_TIPO_TRANSACCION")
+        fecha_contable = req.POST.get("TBK_FECHA_CONTABLE")
+        numero_cuota = req.POST.get("TBK_NUMERO_CUOTAS")
+        #Comprueba archivo
+        try:
+            orden = OrdenCompraWebpay.objects.get(
+                orden_compra=orden_compra)
+            orden.id_transaccion = id_transaccion
+            orden.codigo_transaccion = respuesta
+            orden.codigo_autorizacion = codigo_autorizacion
+            orden.final_num_tarjeta = final_num_tarjeta
+            orden.id_sesion = id_sesion
+            orden.codigo_autorizacion = codigo_autorizacion
+            orden.fecha_transaccion = datetime.today()
+            orden.tipo_pago = tipo_pago
+            orden.tipo_transaccion = tipo_transaccion
+            orden.fecha_contable = fecha_contable
+            orden.numero_cuota = numero_cuota
+        except OrdenCompraWebpay.DoesNotExist:
+            data = [orden_compra, get_client_ip(req)]
+            pago_defectuoso.send(sender=data)
+            raise
+        if respuesta == "0":
+            if int(monto) == int(orden.monto):
+                #Valida MAC
+                if valida_mac(qs) == VALID_MAC_RESPONSE:
+                    orden.status = STATUS["PAGADO"]
+                    resp = ACEPTADO_RESPONSE
+                else:
+                    orden.status = STATUS["MAC_INVALIDO"]
             else:
-                orden.status = STATUS["MAC_INVALIDO"]
+                orden.status = STATUS["MONTO_INVALIDO"]
         else:
-            orden.status = STATUS["MONTO_INVALIDO"]
+            orden.status = STATUS["RESP_INVALIDO"]
+        orden.respuesta = resp
+        orden.save()
+        orden.enviar_signals()
+        return HttpResponse(resp)
     else:
-        orden.status = STATUS["RESP_INVALIDO"]
-    orden.respuesta = resp
-    orden.save()
-    orden.enviar_signals()
-    return HttpResponse(resp)
+        return HttpResponse(resp)
 
 def valida_mac(qs):
     """Funcion que validara la MAC que viene de resultado.cgi. Se debe generar
